4baf932a2bc12adaa4487ada2eff541a08720bc4
Yohendry <yohendry.hurtado@gmail.com>
2023-11-16 15:42:49 -0500

    wip

--- autocmd/init.lua
+++ autocmd/init.lua
@@ -3,3 +3,6 @@ require("user.autocmd.alpha-on-empty")
 -- TODO: configure this code below to remove listchars plugins
 require("user.autocmd.show-white-spaces")
 -- END TODO
+-- TODO: change this auto command to run on lsp tssserver attach
+require("user.autocmd.auto-imports-on-save")
+-- END TODO
